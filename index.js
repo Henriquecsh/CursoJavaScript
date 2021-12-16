4bf659ae455a4f05238347d4ace30a3e7bc4bbf1
Henrique Souza <henrique@webdesigncriarte.com.br>
2021-12-16 01:39:59 -0300

    Curso Javascript orientado a objeto Herança e polimorphismo

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor} from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const conta2 = new ContaCorrente(cliente2, 102);
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-contaCorrenteRicardo.depositar(500);
+const cliente = new Cliente("Lais", 78945612379, "456");
 
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
-
-
-console.log(ContaCorrente.numeroDeContas);
-
+console.log(gerenteEstaLogado);
+console.log(diretorEstaLogado);
+console.log(clienteEstaLogado);
